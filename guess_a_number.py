28e5de0618ae90369eea5d3e5183faedbd026234
Georgi Pavlov <georgi.ivanov.pavlov@gmail.com>
2026-01-17 19:46:22 +0200

    Added difficulty levels
    added 3 difficulty levels; new messages; game over condition

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,6 +2,23 @@ import random
 
 computer_number = random.randint(1, 100)
 
+lives = 0
+
+while True:
+    difficulty = input("Select difficulty [easy / medium / hard]: ").lower()
+
+    if difficulty != "easy" and difficulty != "medium" and difficulty != "hard":
+        print("Invalid choice. Even this part has rules. Try again.")
+    elif difficulty == "easy":
+        lives = 10
+        break
+    elif difficulty == "medium":
+        lives = 7
+        break
+    elif difficulty == "hard":
+        lives = 5
+        break
+
 while True:
     try:
         player_input = input("Guess the number (1-100):")
@@ -11,10 +28,30 @@ while True:
                 print(f"Correct!\nI am impressed. Slightly. Don’t let it get to your head.")
                 break
             elif player_number > computer_number:
+                lives -= 1
+                if lives == 0:
+                    print(
+                        f"Out of lives! The number was {computer_number}. The computer feels achieved and is judging you silently.")
+                    break
                 print("That number has dreams bigger than reality. Try lower.")
             elif player_number < computer_number:
+                lives -= 1
+                if lives == 0:
+                    print(
+                        f"Out of lives! The number was {computer_number}. The computer feels achieved and is judging you silently.")
+                    break
                 print("Too low. Aim higher. I believe in you. A little.")
         else:
+            lives -= 1
+            if lives == 0:
+                print(
+                    f"Out of lives! The number was {computer_number}. The computer feels achieved and is judging you silently.")
+                break
             print(f"Bold choice. Unfortunately, completely out of range.\nTry again...")
     except ValueError:
+        lives -= 1
+        if lives == 0:
+            print(
+                f"Out of lives! The number was {computer_number}. The computer feels achieved and is judging you silently.")
+            break
         print(f"Interesting input. Completely useless, but interesting.\nTry again, this time with a whole number...")
